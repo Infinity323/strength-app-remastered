aadbe485e456e632f5d5ca546c81ecd59f1f5f05
Paul Lee <paul_in_plano@hotmail.com>
2024-03-18 12:40:32 -0500

    adding routes and landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { MatIconButton } from '@angular/material/button';
+import { MatCard, MatCardContent } from '@angular/material/card';
+import { MatIcon } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MyDashboardComponent } from './my-dashboard/my-dashboard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    MyDashboardComponent,
+    MatCard,
+    MatCardContent,
+    MatIcon,
+    MatIconButton,
+    MatListModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    RouterLink,
+    RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { RecordsComponent } from './records/records.component';
+import { MyDashboardComponent } from './my-dashboard/my-dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: MyDashboardComponent },
+  { path: 'calculator', component: CalculatorComponent },
+  { path: 'records', component: RecordsComponent },
+];
