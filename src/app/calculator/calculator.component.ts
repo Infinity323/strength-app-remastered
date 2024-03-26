cb01641e8f420481d3f794c116702543af98f80f
Paul Lee <paul_in_plano@hotmail.com>
2024-03-26 11:44:57 -0500

    rename calculator dialog

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -10,7 +10,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { CalculatorAboutDialogComponent } from './nippard-about-dialog/calculator-about-dialog.component';
+import { CalculatorAboutDialogComponent } from './calculator-about-dialog/calculator-about-dialog.component';
 
 const MAX_WEIGHT: number = 2000;
 const CATEGORIES: string[] = [
